86d22e58e263469a009290c6740656b3aeb13546
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-04-22 08:12:35 +0000

    Add new pages: PersonalRoom, Previous, Recordings, and Upcoming

---
+++ app/(root)/(home)/personal-room/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PersonalRoom = () => {
+  return (
+    <div>Personal-Room</div>
+  )
+}
+
+export default PersonalRoom;
---
+++ app/(root)/(home)/recordings/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Recordings = () => {
+  return (
+    <div>Recording</div>
+  )
+}
+
+export default Recordings;
