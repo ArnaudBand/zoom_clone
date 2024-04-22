d844958dfe53bd98cd2ca7bcd8aecf13aea18e02
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-04-22 08:48:13 +0000

    Fix typo in Personal Room page title

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PersonalRoom = () => {
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
-      <h1 className='text-3xl font-bold'>Persnal Room</h1>
+      <h1 className='text-3xl font-bold'>Personal Room</h1>
     </section>
   )
 }
