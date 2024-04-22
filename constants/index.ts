2f7c353af50a98da20ded1cc9d72c285c2bbb58d
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-04-22 06:09:06 +0000

    Refactor layout components and add Navbar and Sidebar components

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+const sidebarLinks = [
+    {
+        label: 'Home',
+        route: '/',
+        imgUrl: '/icons/something.svg',
+    },    
+    {
+        label: 'Upcoming',
+        route: '/upcoming',
+        imgUrl: '/icons/upcoming.svg',
+    },    
+    {
+        label: 'Previous',
+        route: '/previous',
+        imgUrl: '/icons/prevoious.svg',
+    },    
+    {
+        label: 'Recordings',
+        route: '/recordings',
+        imgUrl: '/icons/video.svg',
+    },    
+    {
+        label: 'Personal Room',
+        route: '/personal-room',
+        imgUrl: '/icons/add-personal.svg',
+    },
+];
