2f83dffda4d13ba01f0a02f4030266e800ac62da
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-04-22 08:38:05 +0000

    Refactor page titles in home directory

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const PersonalRoom = () => {
   return (
-    <div>Personal-Room</div>
+    <section className='flex size-full flex-col gap-10 text-white'>
+      <h1 className='text-3xl font-bold'>Persnal Room</h1>
+    </section>
   )
 }
 
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -23,7 +23,7 @@ const Sidebar = () => {
               <Link
                 key={link.label}
                 href={link.route}
-                className={cn('flex gap-4 items-center p-4 rounded-lg justify-start', {'bg-blue-1': isActive,})}
+                className={cn('flex gap-4 items-center p-4 rounded-lg justify-start', {'bg-blue-500': isActive,})}
                 >
                   <Image src={link.imgUrl} alt={link.label} width={24} height={24} />
                   <span className='text-lg font-semibold max-lg:hidden'>{link.label}</span>
