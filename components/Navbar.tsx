f7e0c35ff16c71550eb231fa58fb56b3d7bd7162
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-04-22 10:14:51 +0000

    Update Navbar and Sidebar components for mobile navigation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,16 +2,22 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
+import MobileNav from './MobileNav';
+import { SignedIn, UserButton } from '@clerk/nextjs';
+
 const Navbar = () => {
   return (
-    <nav className='justify-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+    <nav className='flex justify-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
       <Link href={'/'} className='flex items-center gap-1'>
         <Image src='/icons/logo.svg' alt='logo' width={40} height={40} className='max-sm:size-10' />
         <span className='text-white text-[26px] font-extrabold max-sm:hidden'>Boom</span>
       </Link>
-      <div className='justify-between gap-5'>
+      <div className='flex justify-between gap-5'>
         {/* Clerk - User Management */}
-        Mobile Nav
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <MobileNav />
       </div>
     </nav>
   )
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -17,7 +17,7 @@ const Sidebar = () => {
     >
         <div className='flex flex-col gap-6'>
           {sidebarLinks.map((link) => {
-            const isActive = pathname === link.route || pathname.startsWith(link.route);
+            const isActive = pathname === link.route || pathname.startsWith(`${link.route}/`);
 
             return (
               <Link
