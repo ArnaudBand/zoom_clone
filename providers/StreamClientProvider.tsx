51528b2c5b209722edd77f746c9b864962b6d013
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-04-23 07:13:27 +0000

    🔨 Refactor StreamClientProvider component to use @stream-io/video-react-sdk and add Loader component

--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -1,23 +1,42 @@
+import { ReactNode, useEffect, useState } from 'react';
 import {
-    StreamCall,
     StreamVideo,
     StreamVideoClient,
-    User,
-  } from '@stream-io/video-react-sdk';
-  
-  const apiKey = 'your-api-key';
-  const userId = 'user-id';
-  const token = 'authentication-token';
-  const user: User = { id: userId };
-  
-  const client = new StreamVideoClient({ apiKey, user, token });
-  const call = client.call('default', 'my-first-call');
-  call.join({ create: true });
-  
-  export const App = () => {
+} from '@stream-io/video-react-sdk';
+import { useUser } from '@clerk/nextjs';
+import { tokenProvider } from '@/actions/stream.actions';
+import Loader from '@/components/Loader';
+
+const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+
+const StreamVideoProvider = ({children}: {children: ReactNode}) => {
+    const [videoClient, setVideoClient] = useState<StreamVideoClient>();
+
+    const { user, isLoaded } = useUser();
+
+    useEffect(() => {
+        if (!isLoaded || !user) return;
+        if (!apiKey) throw new Error('Stream API key is not set');
+
+        const client = new StreamVideoClient({
+            apiKey,
+            user: {
+                id: user?.id,
+                name: user?.username || user?.id,
+                image: user?.imageUrl,
+            },
+            tokenProvider,
+        });
+        setVideoClient(client);
+    }, [user, isLoaded]);
+
+    if (!videoClient) return <Loader />;
+    
     return (
-      <StreamVideo client={client}>
-        <StreamCall call={call}>/* My Video UI */</StreamCall>
-      </StreamVideo>
+        <StreamVideo client={videoClient}>
+            {children}
+        </StreamVideo>
     );
-  };
+};
+
+export default StreamVideoProvider;
