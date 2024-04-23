0f5b738548aed067d2a338d5637cc5e83e4de4bf
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-04-23 07:28:45 +0000

    🔧 Update MeetingTypeList component to create a meeting using StreamClientProvider and display meeting details using MeetingModal

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import HomeCard from "./HomeCard";
 import { useRouter } from "next/navigation";
 import MeetingModal from "./MeetingModal";
+import { useUser } from "@clerk/nextjs";
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
 
 const MeetingTypeList = () => {
 
@@ -13,8 +15,48 @@ const MeetingTypeList = () => {
     const [meetingState, setMeetingState] = useState<
         'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>();
 
-    const createMeeting = () => {
-        
+    const [values, setValues] = useState({
+        dateTime: new Date(),
+        description: '',
+        link: ''
+    })
+
+    const [callDetails, setCallDetails] = useState<Call>();
+
+    const { user } = useUser();
+    const client = useStreamVideoClient();
+
+    const createMeeting = async () => {
+        if(!client || !user) return;
+
+        try {
+            const id = crypto.randomUUID();
+
+            const call = client.call('default', id);
+
+            if(!call) throw new Error('Failed to create a call');
+
+            const startsAt = values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+
+            const description = values.description || 'Instant Meeting';
+
+            await call.getOrCreate({
+                data: {
+                    starts_at: startsAt,
+                    custom: {
+                        description
+                    }
+                }
+            })
+
+            setCallDetails(call);
+
+            if(!values.description) {
+                router.push(`/meeting/${call.id}`);
+            }
+        } catch (error) {
+            
+        }
     };
 
     return (
