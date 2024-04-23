45a0e4abf6f564c45e59a2a322feace72ad4765f
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-04-23 20:37:08 +0300

    🔧 Update Meeting component to include StreamCall and StreamTheme components

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,11 +1,34 @@
 "use client";
 
+import React, { useState } from 'react';
 import {useUser} from '@clerk/nextjs';
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import { useGetCallById } from "@/hooks/useGetCallById";
 
-const Meeting = ({ params } : { params: { id: string}}) => {
+import MeetingSetup from "@/components/MeetingSetup";
+import MeetingRoom from "@/components/MeetingRoom";
+import Loader from "@/components/Loader";
+
+const Meeting = ({ params: {id} } : { params: { id: string}}) => {
   const {user, isLoaded}  = useUser();
+  const [isSetupComplete, setIsSetupComplete] =useState(false);
+
+  const { call, isCallLoading } = useGetCallById(id);
+
+  if (!isLoaded || isCallLoading) return <Loader />
+
   return (
-    <div>Meeting Room: #{params.id}</div>
+    <main className="h-screen w-full">
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!isSetupComplete ? (
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+          ): (
+            <MeetingRoom />
+          )}
+        </StreamTheme>
+      </StreamCall>
+    </main>
   )
 }
 
