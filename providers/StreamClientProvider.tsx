7f4facdcdcbd858a43556044704410651b56c43b
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-04-23 06:27:56 +0000

    🔧 Update StreamClientProvider component to use @stream-io/video-react-sdk

--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
-const StreamClientProvider = () => {
-  return (
-    <div>StreamClientProvider</div>
-  )
-}
-
-export default StreamClientProvider;
+import {
+    StreamCall,
+    StreamVideo,
+    StreamVideoClient,
+    User,
+  } from '@stream-io/video-react-sdk';
+  
+  const apiKey = 'your-api-key';
+  const userId = 'user-id';
+  const token = 'authentication-token';
+  const user: User = { id: userId };
+  
+  const client = new StreamVideoClient({ apiKey, user, token });
+  const call = client.call('default', 'my-first-call');
+  call.join({ create: true });
+  
+  export const App = () => {
+    return (
+      <StreamVideo client={client}>
+        <StreamCall call={call}>/* My Video UI */</StreamCall>
+      </StreamVideo>
+    );
+  };
