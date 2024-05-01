3011be63d398b80a59f6c4aed2ca5952958e19c8
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-05-01 20:27:15 +0000

    🔧 Update PersonalRoom component to use Clerk.js for user authentication

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+"use client";
+
+import { useUser } from '@clerk/nextjs';
+import React from 'react';
+
+const Table = ({ title, description }: { title: string; description: string}) => (
+  <div className="flex flex-col items-start gap-2 xl:flex-row">
+    <h1>{title}:</h1>
+    <h1>{description}</h1>
+  </div>
+)
 
 const PersonalRoom = () => {
+
+  const { user } = useUser();
+
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
       <h1 className='text-3xl font-bold'>Personal Room</h1>
+        <div className='flex w-full flex-col gap-8 xl:max-w-[900px]'>
+          <Table title='Topic' description={`${user?.username}'s Meeting Room`} />
+        </div>
     </section>
   )
 }
